2fbe29a9c066955d272b3f826ce4f39e5d6e8dc4
CarlosAGuardado <carlosguardado98@yahoo.com>
2023-05-05 16:29:30 -0400

    major changes 2

---
+++ rental.js
@@ -0,0 +1,13 @@
+const numAdultsEl = document.getElementById('numAdults');
+const numKidsEl = document.getElementById('numKids');
+
+
+const queenBdrEl = document.getElementById('queenBdr');
+const kingBdrEl = document.getElementById('kingBdr');
+const twoBdrEl = document.getElementById('twoBdr');
+
+const aaaEl = document.getElementById('aaaDiscount');
+const mltEl = document.getElementById('mltDiscount');
+
+const btnEl = document.getElementById('submit');
+
